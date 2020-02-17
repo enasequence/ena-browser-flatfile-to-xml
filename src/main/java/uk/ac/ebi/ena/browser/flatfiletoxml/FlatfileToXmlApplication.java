311a733c2b8ef8bea3b283f374ad2e38327671a2
Vishnukumar <kadhirvelu@ebi.ac.uk>
2020-02-17 12:29:23 +0000

    Implementation for loop over the number of entries

--- src/main/java/uk/ac/ebi/ena/browser/flatfiletoxml/FlatfileToXmlApplication.java
+++ src/main/java/uk/ac/ebi/ena/browser/flatfiletoxml/FlatfileToXmlApplication.java
@@ -13,6 +13,7 @@ import uk.ac.ebi.embl.flatfile.writer.xml.XmlEntryWriter;
 
 import javax.validation.constraints.Pattern;
 import java.io.*;
+import java.util.Scanner;
 
 @Slf4j
 @SpringBootApplication
@@ -24,9 +25,14 @@ public class FlatfileToXmlApplication implements CommandLineRunner {
     @Value("${xmlfile}")
     String xmlfile;
 
+    @Value("${file.path}")
+    String filePath;
+
     @Pattern(regexp = "(CDS|EMBL|MASTER|NCR|)")
     @Value("${format:#{null}}")
     String inputFormat;
+
+    private static final String DELIMITER = "//\n";
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(FlatfileToXmlApplication.class);
@@ -37,6 +43,9 @@ public class FlatfileToXmlApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         log.info("Converting file {}", flatfile);
+        flatfile = filePath + flatfile;
+        xmlfile = filePath + xmlfile;
+
         validateArguments();
 
         File file = new File(flatfile);
@@ -45,15 +54,26 @@ public class FlatfileToXmlApplication implements CommandLineRunner {
         if (StringUtils.isNotBlank(inputFormat)) {
             format = EmblEntryReader.Format.valueOf(inputFormat + "_FORMAT");
         }
-        try (
-                BufferedReader fileReader = new BufferedReader(new FileReader(file));
-                BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
-            EmblEntryReader reader = new EmblEntryReader(fileReader,
-                    format, file.getName());
-            reader.read();
-            Entry entry = reader.getEntry();
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
             writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + System.lineSeparator());
-            new XmlEntryWriter(entry).write(writer);
+            writer.write("<ROOT>" + System.lineSeparator());
+            try (Scanner scanner = new Scanner(file)) {
+                scanner.useDelimiter(DELIMITER);
+                while (scanner.hasNext()) {
+                    String s = scanner.next();
+                    if (null != s && !s.equals("")) {
+                        s = s + DELIMITER;
+                        try (BufferedReader stringReader = new BufferedReader(new StringReader(s))) {
+                            EmblEntryReader reader = new EmblEntryReader(stringReader, format, null);
+                            reader.read();
+                            Entry entry = reader.getEntry();
+                            new XmlEntryWriter(entry).write(writer);
+                        }
+                    }
+                }
+            }
+            writer.write("</ROOT>");
             System.out.println("Conversion complete.");
         }
     }
